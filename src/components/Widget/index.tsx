9b04ebc28c6a3f99a7e7a184d6e864e8577f0ca6
Marceometry <marceometry@gmail.com>
2022-05-08 00:16:44 -0300

    fix: css hierarchy

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -10,7 +10,7 @@ export const Widget = () => {
         <WidgetForm />
       </Popover.Panel>
 
-      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+      <Popover.Button className='!bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
